8b5064ac7c3f2c8183b02753f4040fe8a634043f
kariukikinyanjui <michaelkariuki7@gmail.com>
2024-05-14 12:23:36 +0000

    [feat]: define API endpoints for application status and statistics

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,8 @@
-import express from 'express';
-import UsersController from '../controllers/UsersController';
-import AppController from '../controllers/AppController';
-import AuthController from '../controllers/AuthController';
+const express = require('express');
 const router = express.Router();
+const AppController = require('../controllers/AppController');
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
-router.post('/users', UsersController.postNew);
-router.get('/connect', AuthController.getConnect);
-router.get('/disconnect', AuthController.getDisconnect);
-router.get('/users/me', UsersController.getMe);
 
 module.exports = router;
