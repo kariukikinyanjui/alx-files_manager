39b8082340b73b6821aff14af947a3e73ed59f3e
kariukikinyanjui <michaelkariuki7@gmail.com>
2024-05-14 12:20:37 +0000

    [feat]: initialize Express server with dynamic port configuration

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
-const routes = require('./routes/index');
+const routes = require('./routes');
 
-const PORT = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
-app.use('/', routes);
+app.use(express.json());
+app.use('/api', routes);
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${port}`);
 });
