9b62f09e3a54e143bebc63a2a5060e4505446645
kariukikinyanjui <michaelkariuki7@gmail.com>
2024-05-14 01:41:28 +0000

    [feat]: add authentication and user profile endpoints

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import UsersController from '../controllers/UsersController';
 import AppController from '../controllers/AppController';
+import AuthController from '../controllers/AuthController';
 const router = express.Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 router.post('/users', UsersController.postNew);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UsersController.getMe);
 
 module.exports = router;
