93f28a534d589af6b91d67d4a50e0259d36a3787
kariukikinyanjui <michaelkariuki7@gmail.com>
2024-05-14 12:48:07 +0000

    [feat]: add methods to return app status and databases stas

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,17 +1,17 @@
-const dbClient = require('../utils/db');
 const redisClient = require('../utils/redis');
+const dbClient = require('../utils/db');
 
 class AppController {
   static async getStatus (req, res) {
     const redisAlive = redisClient.isAlive();
     const dbAlive = dbClient.isAlive();
-    res.status(200).json({ redis: redisAlive, db: dbAlive });
+    res.status(200).send({ redis: redisAlive, db: dbAlive });
   }
 
   static async getStats (req, res) {
-    const usersCount = await dbClient.nbUsers();
-    const filesCount = await dbClient.nbFiles();
-    res.status(200).json({ users: usersCount, files: filesCount });
+    const users = await dbClient.nbUsers();
+    const files = await dbClient.nbFiles();
+    res.status(200).send({ users, files });
   }
 }
 
